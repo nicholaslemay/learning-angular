7835f0323bee6b7cd2a835658c199bbee3901a04
Nicholas Lemay <lemaynicholas@gmail.com>
2022-03-23 19:38:52 -0400

    Adding Playwright tests

---
+++ tests/example.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect, Page, Request} from '@playwright/test';
+import {GoRestConfig} from "../src/app/shared/goRestConfig";
+
+test.describe('Registration', () => {
+  test('should create user via API', async ({page}) => {
+
+    await page.goto('http://localhost:4200/users/new');
+    await page.locator('h1');
+    await expect(page.locator('h1')).toHaveText('Registration');
+
+    let receivedCall;
+    let receivedRequest;
+    await page.route('**/v2/users**', route => {
+      // @ts-ignore
+      receivedCall = route.request().postDataJSON();
+      receivedRequest = route.request();
+
+      route.fulfill({body: 'mocked-data'});
+    });
+    // Create 1st todo.
+    await page.locator('#name').fill('tony');
+    await page.locator('#email').fill('tony@hotmail.com');
+    await page.locator('#gender').selectOption('female');
+    await page.locator('button[type="submit"]').click();
+
+    expect(receivedCall).toEqual({name: "tony", email: "tony@hotmail.com", gender: "female", status: "active"});
+    // @ts-ignore
+    expect(await receivedRequest.headerValue('authorization')).toEqual(`Bearer ${GoRestConfig.API_TOKEN}`);
+  });
+
+});
